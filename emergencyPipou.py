f3a618327d72a965af880bdb5219c44c45cc1dc8
petosorus <petosorus@hotmail.fr>
2014-12-27 18:54:06 +0100

    Début de réponse à la demande

--- emergencyPipou.py
+++ emergencyPipou.py
@@ -33,68 +33,54 @@ def daemonize():
 
 
 def file_to_dict(file_name):
-	"""opens a file and put tab separated values in a dict"""
-	f = open(file_name, 'r')
-	d_keys = {}
-	for i in f.readlines():
-		i = i[:len(i) - 1]		#Removing the \n char
-		j = i.split('\t')
-		d_keys[j[0]] = j[1]
+    """opens a file and put tab separated values in a dict"""
+    f = open(file_name, 'r')
+    d_keys = {}
+    for i in f.readlines():
+        i = i[:len(i) - 1]        #Removing the \n char
+        j = i.split('\t')
+        d_keys[j[0]] = j[1]
 
-	return d_keys
+    return d_keys
 
 def file_to_probability_dict(file_name):
-	"""opens a file and put tab separated values in a probability dict"""
-	f = open(file_name, 'r')
-	d_words = {}
-	n = 0
-	for i in f.readlines():
-		i = i[:len(i) - 1]		#Removing the \n char
-		j = i.split('\t')
-		num = int(j[0])
-		word = unicode(str(j[1]), 'utf-8')
-		for j in range(int(num)):
-			d_words[n] = word
-			n += 1
-			
-	return d_words
-	
+    """opens a file and put tab separated values in a probability dict"""
+    f = open(file_name, 'r')
+    d_words = {}
+    n = 0
+    for i in f.readlines():
+        i = i[:len(i) - 1]        #Removing the \n char
+        j = i.split('\t')
+        num = int(j[0])
+        word = unicode(str(j[1]), 'utf-8')
+        for j in range(int(num)):
+            d_words[n] = word
+            n += 1
+            
+    return d_words
+    
 def randomized_word(d_words):
-	"""Selects a random word from a probability dict"""
-	p = randrange(len(d_words))
-	return d_words[p]
+    """Selects a random word from a probability dict"""
+    p = randrange(len(d_words))
+    return d_words[p]
 
 def twitter_authentification(keys_file):
-	"""return a tweepy.api object connected to your account"""
-	d_keys = file_to_dict(keys_file)
-	auth = tweepy.OAuthHandler(d_keys["consumer_key"], 
-		d_keys["consumer_secret"])
-	auth.set_access_token(d_keys["access_token"], 
-		d_keys["access_secret"])
-	api = tweepy.API(auth)
-	return api
+    """return a tweepy.api object connected to your account"""
+    d_keys = file_to_dict(keys_file)
+    auth = tweepy.OAuthHandler(d_keys["consumer_key"], 
+        d_keys["consumer_secret"])
+    auth.set_access_token(d_keys["access_token"], 
+        d_keys["access_secret"])
+    api = tweepy.API(auth)
+    return api
 
 def rand_from_list(list_of_things):
-	"""Returns one random item from a set"""
-	return list_of_things[randrange(len(list_of_things))]
+    """Returns one random item from a set"""
+    return list_of_things[randrange(len(list_of_things))]
 
-
-cur_dir = os.path.dirname(os.path.realpath(__file__))
-api = twitter_authentification(cur_dir + "/keys.conf")
-
-seconds_by_day = 86400
-tweets_content = file_to_probability_dict(cur_dir + "/words.conf")
-monday = 0
-
-followers = set(tweepy.Cursor(api.followers).items())
-nb_followers = len(followers)
-messaged_followers = set()
-followers_by_day = ceil(nb_followers / 7.0)
-
-if __name__ == "__main__":
-    retCode = daemonize()
+def fonctionnement_principal():
     while 1:
-        if localtime()[6] == monday:	#Actualize followers list (on monday)
+        if localtime()[6] == monday:    #Actualize followers list (on monday)
             try:
                 followers = set(tweepy.Cursor(api.followers).items())
             except tweepy.error.TweepError:
@@ -102,12 +88,12 @@ if __name__ == "__main__":
             nb_followers = len(followers)
             messaged_followers = set()
             followers_by_day = ceil(nb_followers / 7.0)
-		
+        
         today = localtime()[6]
-	tweets_content = file_to_probability_dict(cur_dir + "/words.conf")
-	
+        tweets_content = file_to_probability_dict(cur_dir + "/words.conf")
+    
         while localtime()[6] == today:
-            time_gap = seconds_by_day / followers_by_day		
+            time_gap = seconds_by_day / followers_by_day        
             to_be_messaged = list(followers - messaged_followers)
 
             #Pick a user to message
@@ -122,5 +108,47 @@ if __name__ == "__main__":
                 api.update_status(message)
             except tweepy.error.TweepError:
                 pass
-		
+            
+            last_mention_id = repondre(mentions)
             sleep(time_gap)
+
+
+def repondre(mentions):
+    try:
+        global last_mention_id = mentions[0]
+    except IndexError:
+        pass
+
+    mentions = api.mentions_timeline(last_mention_id)
+
+    for tweet in mentions:
+
+        handle = "@" + tweet.user.screen_name
+        word = randomized_word(tweets_content)
+        message = handle + " " + word
+
+        api.update_status(message, tweet.id)
+
+    return last_mention_id
+
+
+
+if __name__ == "__main__":
+
+    cur_dir = os.path.dirname(os.path.realpath(__file__))
+    api = twitter_authentification(cur_dir + "/keys.conf")
+
+    seconds_by_day = 86400
+    tweets_content = file_to_probability_dict(cur_dir + "/words.conf")
+    monday = 0
+
+    followers = set(tweepy.Cursor(api.followers).items())
+    nb_followers = len(followers)
+    messaged_followers = set()
+    followers_by_day = ceil(nb_followers / 7.0)
+
+    last_mention_id = None
+    mentions = []
+
+    retCode = daemonize()
+    fonctionnement_principal()
